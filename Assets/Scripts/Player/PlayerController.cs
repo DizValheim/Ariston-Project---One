4d94fdf9318f24463dfbf97866eb34a41c0408bc
DizValheim <122073923+DizValheim@users.noreply.github.com>
2023-10-23 15:18:30 +0530

    Separate player configs using Scriptable Objects

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Ariston.ScriptableObjects;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 
@@ -11,67 +12,9 @@ namespace Ariston
     [RequireComponent(typeof(PlayerInput))]
     public class PlayerController : MonoBehaviour
     {
-        [Header("Player")]
-        [Tooltip("Move speed of the character in m/s")]
-        public float MoveSpeed = 2.0f;
+        [SerializeField] private PlayerSO playerData;
 
-        [Tooltip("Sprint speed of the character in m/s")]
-        public float SprintSpeed = 5.335f;
-
-        [Tooltip("How fast the character turns to face movement direction")]
-        [Range(0.0f, 0.3f)]
-        public float RotationSmoothTime = 0.12f;
-
-        [Tooltip("Acceleration and deceleration")]
-        public float SpeedChangeRate = 10.0f;
-
-        //Audio Section:
-        public AudioClip LandingAudioClip;
-        public AudioClip[] FootstepAudioClips;
-        [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
-
-        [Space(10)]
-        [Tooltip("The height the player can jump")]
-        public float JumpHeight = 1.2f;
-
-        [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
-        public float Gravity = -15.0f;
-
-        [Space(10)]
-        [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
-        public float JumpTimeout = 0.50f;
-
-        [Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
-        public float FallTimeout = 0.15f;
-
-        [Header("Player Grounded")]
-        [Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
-        public bool Grounded = true;
-
-        [Tooltip("Useful for rough ground")]
-        public float GroundedOffset = -0.14f;
-
-        [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
-        public float GroundedRadius = 0.28f;
-
-        [Tooltip("What layers the character uses as ground")]
-        public LayerMask GroundLayers;
-
-        [Header("Cinemachine")]
-        [Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
-        public GameObject CinemachineCameraTarget;
-
-        [Tooltip("How far in degrees can you move the camera up")]
-        public float TopClamp = 70.0f;
-
-        [Tooltip("How far in degrees can you move the camera down")]
-        public float BottomClamp = -30.0f;
-
-        [Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
-        public float CameraAngleOverride = 0.0f;
-
-        [Tooltip("For locking the camera position on all axis")]
-        public bool LockCameraPosition = false;
+        [SerializeField] private GameObject cinemachineCameraTarget;
 
         // cinemachine
         private float _cinemachineTargetYaw;
@@ -88,6 +31,12 @@ namespace Ariston
         // timeout deltatime
         private float _jumpTimeoutDelta;
         private float _fallTimeoutDelta;
+
+        // Additional degress to override the camera. Useful for fine tuning camera position when locked")]
+        private float cameraAngleOverride = 0.0f;
+
+        // For locking the camera position on all axis
+        private bool lockCameraPosition = false;
 
         // animation IDs
         private int _animIDSpeed;
@@ -112,13 +61,11 @@ namespace Ariston
         {
             get
             {
-#if ENABLE_INPUT_SYSTEM
                 return _playerInput.currentControlScheme == "KeyboardMouse";
-#else
-				return false;
-#endif
             }
         }
+
+        public bool Grounded = true;
 
 
         private void Awake()
@@ -132,22 +79,18 @@ namespace Ariston
 
         private void Start()
         {
-            _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
+            _cinemachineTargetYaw = cinemachineCameraTarget.transform.rotation.eulerAngles.y;
             
             _hasAnimator = TryGetComponent(out _animator);
             _controller = GetComponent<CharacterController>();
             _input = GetComponent<PlayerActionMaps>();
-#if ENABLE_INPUT_SYSTEM 
             _playerInput = GetComponent<PlayerInput>();
-#else
-			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
-#endif
 
             AssignAnimationIDs();
 
             // reset our timeouts on start
-            _jumpTimeoutDelta = JumpTimeout;
-            _fallTimeoutDelta = FallTimeout;
+            _jumpTimeoutDelta = playerData.JumpTimeout;
+            _fallTimeoutDelta = playerData.FallTimeout;
         }
 
         private void Update()
@@ -176,9 +119,9 @@ namespace Ariston
         private void GroundedCheck()
         {
             // set sphere position, with offset
-            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset,
+            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - playerData.GroundedOffset,
                 transform.position.z);
-            Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers,
+            Grounded = Physics.CheckSphere(spherePosition, playerData.GroundedRadius, playerData.GroundLayers,
                 QueryTriggerInteraction.Ignore);
 
             // update animator if using character
@@ -191,7 +134,7 @@ namespace Ariston
         private void CameraRotation()
         {
             // if there is an input and camera position is not fixed
-            if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
+            if (_input.look.sqrMagnitude >= _threshold && !lockCameraPosition)
             {
                 //Don't multiply mouse input by Time.deltaTime;
                 float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
@@ -202,17 +145,17 @@ namespace Ariston
 
             // clamp our rotations so our values are limited 360 degrees
             _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
-            _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+            _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, playerData.BottomClamp, playerData.TopClamp);
 
             // Cinemachine will follow this target
-            CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
+            cinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + cameraAngleOverride,
                 _cinemachineTargetYaw, 0.0f);
         }
 
         private void Move()
         {
             // set target speed based on move speed, sprint speed and if sprint is pressed
-            float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
+            float targetSpeed = _input.sprint ? playerData.SprintSpeed : playerData.MoveSpeed;
 
             // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
 
@@ -233,7 +176,7 @@ namespace Ariston
                 // creates curved result rather than a linear one giving a more organic speed change
                 // note T in Lerp is clamped, so we don't need to clamp our speed
                 _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
-                    Time.deltaTime * SpeedChangeRate);
+                    Time.deltaTime * playerData.SpeedChangeRate);
 
                 // round speed to 3 decimal places
                 _speed = Mathf.Round(_speed * 1000f) / 1000f;
@@ -243,7 +186,7 @@ namespace Ariston
                 _speed = targetSpeed;
             }
 
-            _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+            _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * playerData.SpeedChangeRate);
             if (_animationBlend < 0.01f) _animationBlend = 0f;
 
             // normalise input direction
@@ -256,7 +199,7 @@ namespace Ariston
                 _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
                                   _mainCamera.transform.eulerAngles.y;
                 float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
-                    RotationSmoothTime);
+                    playerData.RotationSmoothTime);
 
                 // rotate to face input direction relative to camera position
                 transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
@@ -282,7 +225,7 @@ namespace Ariston
             if (Grounded)
             {
                 // reset the fall timeout timer
-                _fallTimeoutDelta = FallTimeout;
+                _fallTimeoutDelta = playerData.FallTimeout;
 
                 // update animator if using character
                 if (_hasAnimator)
@@ -301,7 +244,7 @@ namespace Ariston
                 if (_input.jump && _jumpTimeoutDelta <= 0.0f)
                 {
                     // the square root of H * -2 * G = how much velocity needed to reach desired height
-                    _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+                    _verticalVelocity = Mathf.Sqrt(playerData.JumpHeight * -2f * playerData.Gravity);
 
                     // update animator if using character
                     if (_hasAnimator)
@@ -319,7 +262,7 @@ namespace Ariston
             else
             {
                 // reset the jump timeout timer
-                _jumpTimeoutDelta = JumpTimeout;
+                _jumpTimeoutDelta = playerData.JumpTimeout;
 
                 // fall timeout
                 if (_fallTimeoutDelta >= 0.0f)
@@ -342,7 +285,7 @@ namespace Ariston
             // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
             if (_verticalVelocity < _terminalVelocity)
             {
-                _verticalVelocity += Gravity * Time.deltaTime;
+                _verticalVelocity += playerData.Gravity * Time.deltaTime;
             }
         }
 
@@ -363,28 +306,28 @@ namespace Ariston
 
             // when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
             Gizmos.DrawSphere(
-                new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z),
-                GroundedRadius);
+                new Vector3(transform.position.x, transform.position.y - playerData.GroundedOffset, transform.position.z),
+                playerData.GroundedRadius);
         }
 
         private void OnFootstep(AnimationEvent animationEvent)
         {
-            if (animationEvent.animatorClipInfo.weight > 0.5f)
-            {
-                if (FootstepAudioClips.Length > 0)
-                {
-                    var index = Random.Range(0, FootstepAudioClips.Length);
-                    AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.TransformPoint(_controller.center), FootstepAudioVolume);
-                }
-            }
+            // if (animationEvent.animatorClipInfo.weight > 0.5f)
+            // {
+            //     if (FootstepAudioClips.Length > 0)
+            //     {
+            //         var index = Random.Range(0, FootstepAudioClips.Length);
+            //         AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.TransformPoint(_controller.center), FootstepAudioVolume);
+            //     }
+            // }
         }
 
         private void OnLand(AnimationEvent animationEvent)
         {
-            if (animationEvent.animatorClipInfo.weight > 0.5f)
-            {
-                AudioSource.PlayClipAtPoint(LandingAudioClip, transform.TransformPoint(_controller.center), FootstepAudioVolume);
-            }
+            // if (animationEvent.animatorClipInfo.weight > 0.5f)
+            // {
+            //     AudioSource.PlayClipAtPoint(LandingAudioClip, transform.TransformPoint(_controller.center), FootstepAudioVolume);
+            // }
         }
     }
 }
---
+++ Assets/Scripts/SO/PlayerSO.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ariston.ScriptableObjects
+{
+    [CreateAssetMenu(fileName ="Player Data", menuName ="Player/Player Data")]
+    public class PlayerSO : ScriptableObject
+    {
+        [Header("Movement")]
+        [SerializeField] private float moveSpeed = 2.0f;
+        [SerializeField] private float sprintSpeed = 5.335f;
+        [SerializeField] private float speedChangeRate = 10.0f;
+        [SerializeField] private float jumpHeight = 1.2f;
+
+        [Tooltip("How fast the character turns to face movement direction")]
+        [Range(0.0f, 0.3f)]
+        [SerializeField] private float rotationSmoothTime = 0.12f;
+
+        [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
+        [SerializeField] private float jumpTimeout = 0.50f;
+
+        [Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
+        [SerializeField] private float fallTimeout = 0.15f;
+
+
+
+        [Header("Look")]
+        [Tooltip("How far in degrees can you move the camera up")]
+        [SerializeField] private float topClamp = 70.0f;
+
+        [Tooltip("How far in degrees can you move the camera down")]
+        [SerializeField] private float bottomClamp = -30.0f;
+
+        
+
+
+
+        [Header("Grounded Check")]
+        [SerializeField] private LayerMask groundLayers;
+        [SerializeField] private float gravity = -15.0f;
+        [Tooltip("Useful for rough ground")]
+        [SerializeField] private float groundedOffset = -0.14f;
+
+        [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
+        [SerializeField] private float groundedRadius = 0.28f;
+
+
+        //Audio Section:
+        // public AudioClip LandingAudioClip;
+        // public AudioClip[] FootstepAudioClips;
+        // [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
+
+        #region Properties
+
+        //Movement
+        public float MoveSpeed => moveSpeed;
+        public float SprintSpeed => sprintSpeed;
+        public float SpeedChangeRate => speedChangeRate;
+        public float RotationSmoothTime => rotationSmoothTime;
+
+        //Jump
+        public float JumpHeight => jumpHeight;
+        public float JumpTimeout => jumpTimeout;
+        public float FallTimeout => fallTimeout;
+
+        //Look
+        public float TopClamp => topClamp;
+        public float BottomClamp => bottomClamp;
+
+        //Grounded Check
+        public LayerMask GroundLayers => groundLayers;
+        public float Gravity => gravity;
+        public float GroundedOffset => groundedOffset;
+        public float GroundedRadius => groundedRadius;
+        
+        #endregion
+        
+    }
+
+}
